3263ffdac69dad5875605e7149072da87f0d6590
Shurlormes <tsdszh@outlook.com>
2018-06-14 15:10:53 +0800

    RedisHttpSession

--- src/main/java/com/voidforce/spring/boot/Application.java
+++ src/main/java/com/voidforce/spring/boot/Application.java
@@ -3,14 +3,14 @@ package com.voidforce.spring.boot;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
+@EnableRedisHttpSession
 @EnableWebSecurity
 @EnableScheduling
-@EnableCaching
 @MapperScan("com.voidforce.spring.boot.mapper")
 public class Application {
 	public static void main(String[] args) {
